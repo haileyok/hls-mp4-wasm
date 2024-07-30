caef9c5ce80c761b024cd0616151dc2fb1f3c776
Hailey <me@haileyok.com>
2024-07-30 11:47:07 -0700

    add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { toBlobURL } from '@ffmpeg/util'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isError, setIsError] = React.useState<boolean>(false)
+  const [mp4, setMp4] = React.useState()
+
+  const ffmpegRef = React.useRef(new FFmpeg())
+
+  const [message, setMessage] = React.useState('')
+
+  const load = React.useCallback(async () => {
+    const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm'
+    const ffmpeg = ffmpegRef.current
+
+    ffmpeg.on('log', ({ message }) => {
+      console.log(message)
+      setMessage(message)
+    })
+
+    await ffmpeg.load({
+        coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+        wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
+    })
+  }, [])
+
+  const convert = React.useCallback(async (videoUrl: string) => {
+    const ffmpeg = ffmpegRef.current
+
+    ffmpeg.exec(['-i', `"${videoUrl}"`, '-codec', 'copy', 'output.mp4'])
+
+    const mp4Data = await ffmpeg.readFile('output.mp4')
+
+    console.log(mp4Data)
+
+    setMessage('done')
+  }, [])
+
+  React.useEffect(() => {
+    const url = new URL(window.location.href)
+    const videoUrl = url.searchParams.get('videoUrl')
+    
+    if (!videoUrl) {
+      setIsError(true)
+    } else {
+      ;(async () => {
+        await load()
+        const mp4Res = await convert(videoUrl)
+        console.log(mp4Res)
+        setMp4(mp4Res)
+      })()
+    }
+  }, [load])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p>message: {message}</p>
+      {isError ? <p>Error!</p> : null}
+    </div>
   )
 }
 
